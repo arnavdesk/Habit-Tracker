8d54b09b810caf768d3626529c26bc2450081d00
arnavdesk <arnavdesk@gmail.com>
2020-06-14 21:06:16 +0530

    add filter option for dates in habbit

--- assets/js/habbit.js
+++ assets/js/habbit.js
@@ -3,8 +3,32 @@
     const daysList = document.getElementById("item-list");
     const url = new URL(window.location.href);
     const habbitId = url.searchParams.get("id");
+    const startDate = document.getElementById("start-date");
+    const endDate = document.getElementById("end-date");
+    const filterBtn = document.getElementById("filter")
+    let currentDate;
+    let sixtyDaysBefore;
     console.log(habbitId);
 
+
+    function SetMinMaxDateOnDatePicker() {
+        currentDate = moment().format('YYYY-MM-DD');
+        sixtyDaysBefore = moment(currentDate).subtract(60, 'days').format('YYYY-MM-DD');
+        console.log(sixtyDaysBefore);
+        startDate.setAttribute("max", currentDate)
+        endDate.setAttribute("max", currentDate);
+        startDate.setAttribute("min", sixtyDaysBefore)
+        endDate.setAttribute("min", sixtyDaysBefore);
+        startDate.value = moment(currentDate).subtract(7, 'days').format('YYYY-MM-DD');
+        endDate.value = currentDate;
+    }
+
+    SetMinMaxDateOnDatePicker();
+
+
+    startDate.onchange = function () {
+        console.log(startDate.value);
+    }
 
     const updateThisDateInDb = async function (date, value) {
         const res = await fetch(`/update-db-date?id=${habbitId}&date=${date}&value=${value}`)
@@ -14,27 +38,33 @@
 
 
 
-    const renderDaysList = function (count, recordTracker) {
+    const renderDaysList = function (count, recordTracker, endDate) {
         let i = 0;
+        // const currentDate = moment().format('YYYY-MM-DD');
         while (i <= count) {
-            const date = moment().subtract(i, 'days').format('DD/MM/YY');
+            const fromattedDate = moment(endDate).subtract(i, 'days').format('LL');
+            const date = moment(endDate).subtract(i, 'days').valueOf() + "";
             const listElement = document.createElement("li");
             listElement.setAttribute("class", "list-item");
             listElement.setAttribute("id", date);
 
             const dateDiv = document.createElement("div");
             dateDiv.setAttribute("class", "date-div");
-            dateDiv.innerHTML = date;
+            dateDiv.innerHTML = fromattedDate;
 
             const statusDiv = document.createElement("div");
             statusDiv.setAttribute("class", "status");
 
             if (date in recordTracker) {
+                console.log(date + "  " + recordTracker[date])
                 if (recordTracker[date] == 0) {
                     statusDiv.style.backgroundColor = "red";
                 }
                 else if (recordTracker[date] == 1) {
                     statusDiv.style.backgroundColor = "green";
+                }
+                else {
+                    statusDiv.style.backgroundColor = "gray";
                 }
             }
             else {
@@ -47,17 +77,17 @@
                 if (this.style.backgroundColor == "gray") {
                     console.log("Hello")
                     this.style.backgroundColor = "green"
-                    value = 1;
+                    value = '1';
                 }
                 else if (this.style.backgroundColor == "green") {
                     console.log("Hello")
                     this.style.backgroundColor = "red"
-                    value = 0;
+                    value = '0';
                 }
                 else if (this.style.backgroundColor == "red") {
                     console.log("Hello")
                     this.style.backgroundColor = "gray"
-                    value = -1;
+                    value = '-1';
                 }
                 updateThisDateInDb(date, value);
             }
@@ -77,15 +107,27 @@
         return data;
     }
 
-    const renderOnLoad = async function () {
+    const renderOnLoad = async function (days, endDate) {
         const data = await fetchFromDB(habbitId);
         const recordTracker = data.record_tracker;
         console.log(recordTracker);
-        renderDaysList(6, recordTracker);
+        renderDaysList(days, recordTracker, endDate);
     }
 
 
+    filterBtn.onclick = function () {
+        let startDateMoment = moment(startDate.value);
+        let endDateMoment = moment(endDate.value);
+        let days = endDateMoment.diff(startDateMoment, 'days');
+        if (days < 0) {
+            alert("Start date cannot be greater than end date");
+            return;
+        }
+        daysList.innerHTML = "";
+        renderOnLoad(days, endDateMoment);
+    }
 
-    renderOnLoad();
+
+    renderOnLoad(6, currentDate);
 
 })();
